7a3bc13a16a40f1deeed33c67b8235d311a9975e
marwanabdelwahab9@gmail.com <marwanabdelwahab9@gmail.com>
2023-05-09 03:21:00 +0200

    Edit Filter

--- src/Component/Shop/Products.jsx
+++ src/Component/Shop/Products.jsx
@@ -22,72 +22,88 @@ export default function Products() {
   );
   const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
 
-
   return (
     <div>
       {" "}
       <div>
         <div className="row mt-4">
           <div className="row mt-4">
-            {filteredData.length === 0 ? (
+            {selectedBrand.length ||
+            selectedModel.length ||
+            selectedYear.length > 0 ? (
               <>
-                {Data.map((product) => (
-                  <div className="col-md-6  col-sm-12 gy-3 bg-    p-2  product-container ">
-                    <div className="product">
-                      <div className="row gx-2">
-                        <div className="col-md-5 col-5">
-                          <div className="img-product">
-                            <img
-                              src={`${
-                                product.ImgSrc == null
-                                  ? require("../../images/product.png")
-                                  : product.ImgSrc
-                              }`}
-                              alt=""
-                              className="w-100"
-                            />
-                          </div>{" "}
-                        </div>
-                        <div className="col-md-7 col-7">
-                          {" "}
-                          <div className="desc-product">
-                            <div className="d-flex justify-content-between">
-                              <p className="product-in-stock">
-                                {product.countInStock}{" "}
-                                <span className="text-muted">In stock</span>
-                              </p>
-                              <h5 className="product-price">
-                                {product.Price} L.E{" "}
-                              </h5>
+                {!filteredData.length ? (
+                  <div>
+                    <h1>Product Doesn't Exist</h1>{" "}
+                  </div>
+                ) : (
+                  <div>
+                    {" "}
+                    {currentItems.map((product) => (
+                      <div className="col-md-6  col-sm-12 gy-3 bg-    p-2  product-container ">
+                        <div className="product">
+                          <div className="row gx-2">
+                            <div className="col-md-5 col-5">
+                              <div className="img-product">
+                                <img
+                                  src={`${
+                                    product.ImgSrc == null
+                                      ? require("../../images/product.png")
+                                      : product.ImgSrc
+                                  }`}
+                                  alt=""
+                                  className="w-100"
+                                />
+                              </div>{" "}
                             </div>
-                            <h4 className="product-name">
-                              {product.Product_Name}
-                            </h4>
-                            <p className="product-details">
-                              {product.Product_Details}
-                            </p>
-                            <div className="d-flex justify-content-between">
-                              <button className="bton">
-                                <p className="btn-desc">{product.Car_Model}</p>
-                              </button>
-                              <button className="bton">
-                                <p className="btn-desc">{product.Car_Year}</p>
-                              </button>
-                              <button className="bton">
-                                <p className="btn-desc">{product.Car_Brand}</p>
-                              </button>
+                            <div className="col-md-7 col-7">
+                              {" "}
+                              <div className="desc-product">
+                                <div className="d-flex justify-content-between">
+                                  <p className="product-in-stock">
+                                    {product.countInStock}{" "}
+                                    <span className="text-muted">In stock</span>
+                                  </p>
+                                  <h5 className="product-price">
+                                    {product.Price} L.E{" "}
+                                  </h5>
+                                </div>
+                                <h4 className="product-name">
+                                  {product.Product_Name}
+                                </h4>
+                                <p className="product-details">
+                                  {product.Product_Details}
+                                </p>
+                                <div className="d-flex justify-content-between">
+                                  <button className="bton">
+                                    <p className="btn-desc">
+                                      {product.Car_Model}
+                                    </p>
+                                  </button>
+                                  <button className="bton">
+                                    <p className="btn-desc">
+                                      {product.Car_Year}
+                                    </p>
+                                  </button>
+                                  <button className="bton">
+                                    <p className="btn-desc">
+                                      {product.Car_Brand}
+                                    </p>
+                                  </button>
+                                </div>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                    </div>
+                    ))}
                   </div>
-                ))}
+                )}
               </>
             ) : (
               <>
                 {" "}
-                {currentItems.map((product) => (
+                {Data.map((product) => (
                   <div className="col-md-6  col-sm-12 gy-3 bg-    p-2  product-container ">
                     <div className="product">
                       <div className="row gx-2">
